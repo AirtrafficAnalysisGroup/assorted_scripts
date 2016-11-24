0e87ed04467d40e7f1a55e17a19112576a90af81
Ruslan Shaydulin <rshaydu@node1146.palmetto.clemson.edu>
2016-11-24 17:38:26 -0500

    Added script to merge lookup tables

--- two_lookup_tables/script_to_merge.py
+++ two_lookup_tables/script_to_merge.py
@@ -11,7 +11,7 @@ def fill_dict(csv_reader):
 		out_dict[key].append((row[2],row[0]))
 	return out_dict
 
-def compare_states(st1, st2):
+def compare_strings(st1, st2):
 	return (st1 == st2) or (st1 in st2) or (st2 in st1)
 
 with open('bls_lookup_sm.area.csv', 'r') as ec_table, open('bts_filtered_3_col.csv', 'r') as fl_table, open('merged_lookup.csv', 'w+') as outfile:
@@ -21,20 +21,20 @@ with open('bls_lookup_sm.area.csv', 'r') as ec_table, open('bts_filtered_3_col.c
 	fl_dict = fill_dict(fl_reader) 
 	
 	#go through ec_dict (because it's bigger) and try to find corresponding value in fl_dict, if found – write to file
-	for key in ec_dict:
-		#first, check if the same city simply exists in the ec_dict
-		if key in fl_dict:
-			if ((len(fl_dict) == 1) and (len(ec_dict) == 1)):
-				#no collisions 
-				str_out = key + ',' + ','.join(ec_dict[key]) + ',' + ','.join(fl_dict[key]) + '\n'
-				outfile.write(str_out)
-			else:
-				#handle collision. Look at all combinations of pairs (state, number) for this key
-				for fl_p in fl_dict[key]:
-					for ec_p in ec_dict[key]:
-						fl_st, fl_num = fl_p
-						ec_st, ec_num = ec_p
-						if compare_states(fl_st, ec_st):
-							str_out = key + ','+ ec_st + ',' + ec_num + ',' + fl_st + ',' + fl_num + '\n'
-							outfile.write(str_out)
+	for ec_key in ec_dict:
+		for fl_key in list(fl_dict):
+			if compare_strings(ec_key, fl_key):
+				if ((len(fl_dict) == 1) and (len(ec_dict) == 1)):
+					#no collisions 
+					str_out = ec_key + ',' + ','.join(ec_dict[ec_key]) + ',' + fl_key + ','  + ','.join(fl_dict[fl_key]) + '\n'
+					outfile.write(str_out)
+				else:
+					#handle collision. Look at all combinations of pairs (state, number) for this ec_key
+					for fl_p in fl_dict[fl_key]:
+						for ec_p in ec_dict[ec_key]:
+							fl_st, fl_num = fl_p
+							ec_st, ec_num = ec_p
+							if compare_strings(fl_st, ec_st):
+								str_out = ec_key + ','+ ec_st + ',' + ec_num + ',' + fl_key + ',' + fl_st + ',' + fl_num + '\n'
+								outfile.write(str_out)
 						
